44b40877d517ffc90e18c02b165fa7c59c5d708e
Nikita Tulenev <n1tulenev@gmail.com>
2026-02-26 19:36:45 +0100

    Move ReportConfigOptions to separate file

--- src/Models/Configuration/JiraOptions.cs
+++ src/Models/Configuration/JiraOptions.cs
@@ -39,34 +39,3 @@ internal sealed class JiraOptions
     /// </summary>
     public IReadOnlyList<ReportConfigOptions>? Reports { get; init; }
 }
-
-/// <summary>
-/// Represents one raw report config entry from configuration.
-/// </summary>
-internal sealed class ReportConfigOptions
-{
-    /// <summary>
-    /// Gets config name.
-    /// </summary>
-    public string? Name { get; init; }
-
-    /// <summary>
-    /// Gets config JQL query.
-    /// </summary>
-    public string? Jql { get; init; }
-
-    /// <summary>
-    /// Gets requested output fields.
-    /// </summary>
-    public IReadOnlyList<string>? OutputFields { get; init; }
-
-    /// <summary>
-    /// Gets requested grouped count fields.
-    /// </summary>
-    public IReadOnlyList<string>? CountFields { get; init; }
-
-    /// <summary>
-    /// Gets required PDF report title/file base name.
-    /// </summary>
-    public string? PdfReportName { get; init; }
-}
